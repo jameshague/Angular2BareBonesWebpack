3f91d8611decfb066f3126d6b546e0ca17d0d852
James Hague <James Hague>
2017-03-06 20:59:31 +0000

    added support for CSS

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,12 @@ import {Component} from '@angular/core'
 
 @Component({
     selector: 'app-component',
-    templateUrl: 'app.component.html'
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css']
 })
 export class AppComponent{
 
 }
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var helpers = require('./helpers')
+var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './app/main.ts',
+    devtool: 'inline-source-map',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
@@ -13,16 +16,26 @@ module.exports = {
         rules: [
             {
                 test: /\.ts$/,
-                loaders: ['awesome-typescript-loader', 'angular2-template-loader'],                
+                loaders: ['awesome-typescript-loader', 'angular2-template-loader'],
                 exclude: [/\.(spec|e2e)\.ts$/]
             },
             {
                 test: /\.css$/,
-                loaders: 'style-loader!css-loader'
+                exclude: path.resolve(__dirname, 'app'),
+                loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: 'css-loader?sourceMap' })
+            },
+            {
+                test: /\.css$/,
+                include: path.resolve(__dirname, 'app'),
+                loader: 'raw-loader'
+            },
+            {
+                test: /\.jpg$/,
+                loaders: 'file-loader'
             },
             {
                 test: /\.html$/,
-                loader: 'raw-loader'
+                loader: 'html-loader'
             }
         ],
     },
@@ -30,7 +43,12 @@ module.exports = {
     [
         new HtmlWebpackPlugin({
             template: 'index.html'
-        })
+        }),
+        new ExtractTextPlugin('[name].css')
+
+        // ,new BundleAnalyzerPlugin({
+        //     analyzerMode: 'static'
+        // })
     ],
     resolve: {
         extensions: [".ts", ".js"]
